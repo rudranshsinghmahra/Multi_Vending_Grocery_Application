4ce3e711a34dfbb6bad2786afe2de537a34e6e01
rudranshsinghmahra <rs.mahra4546@gmail.com>
2022-02-18 19:13:00 +0530

    added_distance_between_user_and_vendor

--- lib/screens/top_picked_store.dart
+++ lib/screens/top_picked_store.dart
@@ -15,92 +15,131 @@ class TopPickedStore extends StatefulWidget {
 
 class _TopPickedStoreState extends State<TopPickedStore> {
   final StoreServices _storeServices = StoreServices();
-  UserServices _userServices = UserServices();
+  final UserServices _userServices = UserServices();
   User? user = FirebaseAuth.instance.currentUser;
   var _userLatitude = 0.0;
   var _userLongitude = 0.0;
 
   @override
   void initState() {
-    _userServices.getUserDataById(user!.uid).then((value){
-      if(user!=null){
-        setState(() {
-          _userLatitude = value.data()['latitude'];
-          _userLongitude = value.data()['longitude'];
-        });
-      }else{
+    _userServices.getUserDataById(user!.uid).then((value) {
+      if (user != null) {
+       if(mounted){
+         setState(() {
+           _userLatitude = value['latitude'];
+           _userLongitude = value['longitude'];
+         });
+       }
+      } else {
         Navigator.pushReplacementNamed(context, WelcomeScreen.id);
       }
     });
     super.initState();
   }
 
+  String getDistance(location) {
+    var distance = Geolocator.distanceBetween(
+        _userLatitude, _userLongitude, location.latitude, location.longitude);
+    var distanceInKm = distance/1000;
+    return distanceInKm.toStringAsFixed(2);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: StreamBuilder<QuerySnapshot>(
+    return StreamBuilder<QuerySnapshot>(
       stream: _storeServices.getTopPickedStore(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (!snapshot.hasData) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        List actualShopDistance = [];
-        for(int i = 0;i<= snapshot.data!.docs.length;i++){
-          var distance = Geolocator.distanceBetween(startLatitude, startLongitude, endLatitude, endLongitude);
-        }
-        return Column(
-          children: [
-            Flexible(
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: snapshot.data!.docs.map(
-                  (DocumentSnapshot document) {
-                    return Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: SizedBox(
-                        width: 80,
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              width: 80,
-                              height: 80,
-                              child: Card(
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(4),
-                                  child: Image.network(
-                                    document['imageUrl'],
-                                    fit: BoxFit.cover,
-                                  ),
+    if (!snapshot.hasData) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    List actualShopDistance = [];
+    for (int i = 0; i <= snapshot.data!.docs.length-1; i++) {
+      var distance = Geolocator.distanceBetween(
+        _userLatitude,
+        _userLongitude,
+        snapshot.data?.docs[i]['location'].latitude,
+        snapshot.data?.docs[i]['location'].longitude,
+      );
+      var distanceInKm = distance/1000;
+      actualShopDistance.add(distanceInKm);
+    }
+    actualShopDistance.sort(); // this will sort with nearest distance. If nearest distance
+    if(actualShopDistance[0]>10){
+      return Column(
+        children: const [
+          Padding(
+            padding: EdgeInsets.only(left: 20,top: 20),
+            child: Text("No Store Within 10km",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+          )
+        ],
+      );
+    }
+    return Column(
+      children: [
+        Flexible(
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: snapshot.data!.docs.map(
+              (DocumentSnapshot document) {
+                if(double.parse(getDistance(document['location']))<=10){
+                  return Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: SizedBox(
+                      width: 80,
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            width: 80,
+                            height: 80,
+                            child: Card(
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(4),
+                                child: Image.network(
+                                  document['imageUrl'],
+                                  fit: BoxFit.cover,
                                 ),
                               ),
                             ),
-                            SizedBox(
-                              height: 40,
-                              child: Text(
-                                document['shopName'],
-                                style: const TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold),
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                                textAlign: TextAlign.center,
-                              ),
+                          ),
+                          SizedBox(
+                            height: 40,
+                            child: Text(
+                              document['shopName'],
+                              style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.center,
                             ),
-                            const Text("20Km",style: TextStyle(
-                              color: Colors.grey,fontSize: 14
-                            ),),
-                          ],
-                        ),
+                          ),
+                          Text(
+                            "${getDistance(document['location'])}km",
+                            style: const TextStyle(
+                                color: Colors.grey, fontSize: 14),
+                          ),
+                        ],
                       ),
-                    );
-                  },
-                ).toList(),
-              ),
-            )
-          ],
-        );
+                    ),
+                  );
+                }else{
+                  return Column(
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.only(left: 20,top: 20),
+                        child: Text("No Store Within 10km",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                      )
+                    ],
+                  );
+                }
+              },
+            ).toList(),
+          ),
+        ),
+      ],
+    );
       },
-    ));
+    );
   }
 }
